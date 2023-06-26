473cb0a8202899624c9b84dc445f548cbddd07e3
BiswasAri10 <bis.ips21@gmail.com>
2023-06-26 20:52:28 +0530

    Error modal problem fixed

--- src/Components/UI/ErrorModal.js
+++ src/Components/UI/ErrorModal.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { ReactDOM } from "react";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import Card from "./Card";
-import Button from "./Button";
-import classes from "./ErrorModal.module.css";
+import Card from './Card';
+import Button from './Button';
+import classes from './ErrorModal.module.css';
 
 const Backdrop = (props) => {
   return <div className={classes.backdrop} onClick={props.onConfirm} />;
@@ -29,16 +29,16 @@ const ErrorModal = (props) => {
   return (
     <React.Fragment>
       {ReactDOM.createPortal(
-        <Backdrop onConfirm={props.onConfirm}></Backdrop>,
-        document.getElementById("backdrop-root")
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById('backdrop-root')
       )}
       {ReactDOM.createPortal(
         <ModalOverlay
           title={props.title}
           message={props.message}
           onConfirm={props.onConfirm}
-        ></ModalOverlay>,
-        document.getElementById("overlay-root")
+        />,
+        document.getElementById('overlay-root')
       )}
     </React.Fragment>
   );
